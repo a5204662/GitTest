539048b2ac33ef39910e4bcc9c30a1428240384a
a5204662 <976525070@qq.com>
2018-04-21 15:23:49 +0800

    add some new file

---
+++ NLP/net/src/input_point.h
@@ -0,0 +1,10 @@
+#ifndef INPUTPOINT20180421
+#define INPUTPOINT20180421
+
+#include "point.h"
+
+class InputPoint : public Point {
+    
+};
+
+#endif //INPUTPOINT20180421
---
+++ NLP/net/src/point.h
@@ -0,0 +1,42 @@
+#ifndef POINT20180421
+#define POINT20180421
+
+#include <stdint.h>
+#include <cstring>
+#include <vector.h>
+
+class Point{
+public:
+    double GetX(uint32_t index) const {
+        return m_xs[index];
+    }
+    double GetB() const {
+        return m_b;
+    }
+    double GetY() const {
+        return m_y;
+    }
+
+    void SetX(std::vector<double> xsIn) {
+        m_xs = xsIn;
+    }
+    void SetB(double bIn) {
+        m_b = bIn;
+    }
+    void SetY(double yIn) {
+        m_y = yIn;
+    }
+
+    void ToString() const {
+        for(uint32_t i = 0; i < m_xs.size(); ++i) {    
+            printf("x%u : %f, ", i, m_xs[i]);
+        }
+        printf("b : %f, y : %f\n", m_b, m_y);
+    }
+private:
+    std::vector<double> m_xs;
+    std::double m_b;
+    std::double m_y;
+};
+
+#endif //POINT20180421
